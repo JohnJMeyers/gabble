2c6364d63158fba72f6a9f4ee9584214ccf6e153
John Meyers <johnjmeyers1@gmail.com>
2017-06-30 12:32:11 -0500

    add express requirements, add login page, add session

---
+++ migrations/20170630033637-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      likes: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      messages: {
+        type: Sequelize.TEXT
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    likes: DataTypes.ARRAY(DataTypes.STRING),
+    messages: DataTypes.TEXT,
+    password: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
